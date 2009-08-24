1f1ae14acc634288f3d9b808127529605703f6c5
root <root@dhcp17.asianux.net>
2009-08-25 07:26:50 +0800

    petu

---
+++ petu/creatrepo.c
@@ -0,0 +1,2 @@
+so what it does is to generate several xml file. so first thing I need to do is to write a downloader to download the
+most important one primary.xml.
---
+++ petu/intro.c
@@ -0,0 +1,6 @@
+petu==peter's updater
+
+I want to use yum repo, which means the server side repo is created with creatrepo,
+is there any project that has the C++ code to manipulate these xml files.
+
+I checked apt-rpm, but is does not use yum repo at all.
