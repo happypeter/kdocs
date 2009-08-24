61627ae3d6dd164ee8551a799085680112207e8d
Peter King <happypeter1983@gmail.com>
2009-08-24 14:06:14 +0800

    qwt

---
+++ study/qt/qwt/compile.c
@@ -0,0 +1,3 @@
+Now I have qwt-5.2/
+1. go to top dir run make, the generate libqwt, export it location in LD_LIBRARY_PATH
+2. then go to examples/, qmake&&make, you will find the binary under ./bin/
