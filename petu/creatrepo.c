54b89bcb054155a8a63dce9599e2631f6daeb9bd
Peter King <happypeter1983@gmail.com>
2009-09-02 09:57:32 +0800

    d

--- petu/creatrepo.c
+++ petu/creatrepo.c
@@ -1,5 +1,5 @@
 so what it does is to generate several xml file. so first thing I need to do is to write a downloader to download the
-most important one primary.xml.
+most important one is primary.xml.
 
 maybe I need libxml http://xmlsoft.org/
 
