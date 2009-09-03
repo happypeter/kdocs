0c3c687cc107b77e6c8d32e0f726dc7dbd0f7225
root <root@dhcp17.asianux.net>
2009-09-04 03:28:29 +0800

    wget

--- petu/wget.c
+++ petu/wget.c
@@ -21,5 +21,15 @@ int classNetwork::GetHeader()
                 m_clsDown->setTimestamping();
                 m_clsDown->setUrl(url);
                 err_value=m_clsDown->getFile();
-        /*
+        
 //////////////////============
+
+===
+since it looks stupid to have wget tree in axtu tree, so I want to remove it.
+retrieve_url() is actually what is mainly used in axtu, but when I hacking down into wget/
+I soon understand it is not so easy to drag this function out, since it depends on two many other files, 
+so it is even cleaner to have wget used as a whole rather then have many changed wget files.
+===
+in classDownloader.h, there is even
+#include <wget/config.h>
+so I need to study GNU autotools a liitle again
