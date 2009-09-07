2ba2db14e260a0a45ee2844e746a864a3bfeae8a
Peter King <happypeter1983@gmail.com>
2009-09-08 07:19:57 +0800

    d

---
+++ doc-axtu/auth-client/1.c
@@ -0,0 +1,6 @@
+one thing hwanii menationed is interesting,
+he said auth_mysql wants AK and TK to unlock the repo.
+that's why we believe unauthorized user can not use TSN to download packages.
+but if auth-client is open source, then people can hack down to find out how the
+hardware hash can all other local info is generated, save them, use them one another machine
+to cheat auth_mysql this machine is also authorized. That's possible, I think.
