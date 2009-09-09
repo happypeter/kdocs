4c472946b7d90d901b4d7c8079d1aaed1e823c0f
Peter King <happypeter1983@gmail.com>
2009-09-10 03:30:28 +0800

    xx

---
+++ doc-axtu/bugs/make_install.c
@@ -0,0 +1,5 @@
+bug: when I debug libs/, every time I am done, I can simply run make install and test it.
+but gui/ stuff simply can not be updated by make install, no matter you run command in top dir or sub dir.
+
+FIX: so it is done by rpm, you can check the SPEC
+
