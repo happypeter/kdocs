71a31c38447e33626e666464a1789b3c52887cb2
Peter King <happypeter1983@gmail.com>
2009-09-14 15:44:34 +0800

    d

--- petu/creatrepo.c
+++ petu/creatrepo.c
@@ -2,6 +2,7 @@ so what it does is to generate several xml file. so first thing I need to do is 
 most important one is primary.xml.
 
 maybe I need libxml http://xmlsoft.org/
+To prase  XML files, try Qt XQUERY, doc available in Qt creator
 
 see what we have on asianux 
 rpm -ql libxml2-devel
