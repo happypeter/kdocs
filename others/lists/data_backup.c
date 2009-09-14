8cbc71d48a7e3b7b17af52b3bfe499bab27c9131
Peter King <happypeter1983@gmail.com>
2009-09-14 10:41:30 +0800

    data_backup.c

---
+++ others/lists/data_backup.c
@@ -0,0 +1,11 @@
+Now I finally have two computers(my laptop and desktop) and I have md5sum
+so I can do data backup finally.
+
+1. things to backup
+	1.1 kdocs will be kept in github
+	1.2 there will be a list in kdocs for my data_backup, I mean big things
+	1.3 music
+		+++photo(local+flicker)
+		+++project data(vivi arm-linux-gcc ...)
+		+++pdf-books
+		+++others
