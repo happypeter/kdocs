8b10e7416cdf4bcb908cda3ae8c14092f74a1d32
Peter King <happypeter1983@gmail.com>
2009-09-24 11:34:28 +0800

    add more

--- doc-axtu/axtu/obsolete.c
+++
@@ -1,5 +0,0 @@
-to implement axtu obsolete tag support, I need to read out "obsoletes" from server-side hdr files.
-Last time when I read epoch, it is in local rpmdb. but they are not so different!
-All I need to do is to readout info from Header.
-    RPMTAG_OBSOLETENAME  
-
