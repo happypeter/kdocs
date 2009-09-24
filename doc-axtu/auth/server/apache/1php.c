784b40f869685e8d0981197017f39f3001b08478
Peter King <happypeter1983@gmail.com>
2009-09-24 11:10:02 +0800

    make html docs

--- doc-axtu/auth/server/apache/1php.c
+++
@@ -1,1 +0,0 @@
-http://www.swig.org/Doc1.3/Php.html
--- doc-axtu/auth/server/apache/asianux2.c
+++
@@ -1,5 +0,0 @@
-now I am trying to change the repo path 
-form ../asianux2/.. to ../asianux/..
-1. ln -s asianux2 asianux
-2. change connect_info asianux2->asianux
-3. done
