59b75fd2e5578d64a04a7055e23534a3a975654c
Peter King <happypeter1983@gmail.com>
2009-09-29 07:14:59 +0800

    debug info to bugzilla or doc-axtu

--- axtu-debug/all-older/tui/1.c
+++
@@ -1,8 +0,0 @@
->>>I am in launcher/tui/
-1. newt_unloading()
-if you delete this you can never stop the tui
-2. add ...
->>>http://www.whoow.org/people/Jean-Marc.Lienher/gnewt/tutorial-2.html
-$ gcc test.c -lnewt
-
-newtWinMessage-------see the gnewt sample hello.c to know this function
