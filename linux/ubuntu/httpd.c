0519bce622fcb148a65bd530f5767bae68d273c9
pet <pet@girl.(none)>
2009-10-22 11:27:01 +0800

    d

--- linux/ubuntu/httpd.c
+++ linux/ubuntu/httpd.c
@@ -1,4 +1,29 @@
+= ubuntu 9.04 =
+
+
 apt-get install apache2
 /var/www/index.html
 http://127.0.0.1/
 done
+
+
+== cgi-bin ==
+{{{
+vim /etc/apache2/sites-enabled/000-default
+}}}
+
+{{{
+        ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/
+        <Directory "/usr/lib/cgi-bin">
+                AllowOverride None
+                Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+                Order allow,deny
+                Allow from all
+        </Directory>
+}}}
+
+then I can put s.py into /usr/lib/cgi-bin/, can call it like this
+{{{
+
+<form method=POST action="cgi-bin/s.py">
+}}}
--- linux/ubuntu/short-cuts.c
+++
@@ -1,4 +0,0 @@
-Clt+Win+D ----------close a terminal tab
-shift+clt+t---------open a termil tab
-clt+w---------------close a Firefox tab
-clt+t---------------open a firefox tab
