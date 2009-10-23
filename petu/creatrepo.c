02453e528afac2f5f27e0d2c9779d6c88ce6caa7
pet <pet@pet-desktop.(none)>
2009-10-23 16:23:10 +0800

    rm petu/, since I think it is not very useful

--- petu/creatrepo.c
+++
@@ -1,8 +0,0 @@
-so what it does is to generate several xml file. so first thing I need to do is to write a downloader to download the
-most important one is primary.xml.
-
-maybe I need libxml http://xmlsoft.org/
-To prase  XML files, try Qt XQUERY, doc available in Qt creator
-
-see what we have on asianux 
-rpm -ql libxml2-devel
